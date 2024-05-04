413230eaf7450fa146348b9e8efe4579789da881
Rutwik Chaudhari <rutwik.chaudhari@gmail.com>
2024-05-03 17:06:15 -0700

    Setup and Header

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,31 @@
+import { relative } from "path";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Rutwik Chaudhari | Personal Portfolio",
+  description: "Rutwik is a Full Stack Developer with 2 years of experience",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div
+          className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] 
+        w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        <Header></Header>
+        {children}
+      </body>
+    </html>
+  );
+}
